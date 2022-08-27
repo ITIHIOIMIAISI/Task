1fdc92c16c992502ed608375914878006e00333c
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-28 00:34:12 +0300

    Init Task42

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,25 +1,6 @@
-﻿/* Задача 40: 
-Напишите программу, которая принимает на вход три числа и проверяет, 
-может ли существовать треугольник с сторонами такой длины.Теорема о неравенстве 
-треугольника: каждая сторона треугольника меньше суммы двух других сторон.  */
+﻿/* Задача 42: 
+Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10 */
 
-Console.WriteLine("Введите строну а  ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите строну b  ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите строну c  ");
-int c = Convert.ToInt32(Console.ReadLine());
-
-bool CheckTriangle(int num1, int num2, int num3)
-{
-    return num1+num2>num3 && num2+num3>num1 && num1+num3>num2;
-}
-
-if(CheckTriangle(a, b, c))
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
