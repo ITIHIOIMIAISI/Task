1499815e68866082c0e39dbc6f6e0b73b85cfa43
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-28 18:21:54 +0300

    Task44

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,23 +1,41 @@
-﻿/* Задача 42: 
-Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-45 -> 101101
-3  -> 11
-2  -> 10 */
+﻿/* Задача 44: 
+Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
 
-Console.WriteLine("Введите число:  ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int GetBinary(int num)
+int Fibonacci(int n)
 {
-    string result ="";
-    while (num > 0)
+    int resut = 0;
+    int resut2 = 1;
+    int temp = 0;
+    for(int i=1; i < n; i++)
     {
-        result = result + Convert.ToString(num % 2);
-        num /= 2;
+        Console.Write(resut + " ");
+        temp = resut2;
+        resut2 += resut;
+        resut = temp;
     }
-    result.ToCharArray().Reverse();
-
-    return int.Parse(result.ToCharArray().Reverse());
+    return (resut);
 }
 
-Console.WriteLine($"{GetBinary(number)}");
+void FibonacciSeminar(int n)
+{
+    int[] arr = new int[n];
+    arr[0] = 0;
+    arr[1] = 1;
+    for(int i=2; i < n; i++)
+    {
+        arr[i] = arr[i-1]+arr[i-2];
+    }
+    var str = string.Join(" ", arr);
+    Console.WriteLine($"Первые {n} чисел Фабоначчи   {str}");
+}
+
+
+
+Console.WriteLine("Enter a number");
+int n = Convert.ToInt32(Console.ReadLine());
+/* Console.WriteLine($"{Fibonacci(n)}"); */
+Console.WriteLine($"{Fibonacci(n)}");
+FibonacciSeminar(n);
