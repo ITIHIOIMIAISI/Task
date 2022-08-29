6fc064dfd950a554ceedb4eb42bceb7e8726b981
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-29 22:32:37 +0300

    Task 43 (three days of suffering)

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,21 +1,33 @@
-﻿/* Задача 41: 
-Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. */
+﻿/* Задача 43. 
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. */ 
 
-float GetNumbersAboveZero(float sum)
+
+Console.Write("k1  ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1  ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("k2  ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2  ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+if ((k1 == k2) && (b1 == b2))
 {
-    int count = 0;
-    for(int i=0; i<sum; i++)
-    {
-        Console.WriteLine("Enter a digit");
-        float number = (Convert.ToInt32(Console.ReadLine()));
-            if (number>0)
-            {
-                count++;
-            }
-    }
-    return(count);
+        Console.WriteLine("Координаты прямых совпадают"); 
 }
-
-Console.WriteLine("Enter a nubmer");
-float sum = (Convert.ToInt32(Console.ReadLine()));
-Console.WriteLine($"the Quantity of numbers above 0 {GetNumbersAboveZero(sum)}");
+else
+{
+    if ((k1 == k2) || (b1 == b2))
+    {
+        Console.WriteLine("Координаты прямых паралельны");   
+    }
+    else
+    {
+        x = Math.Round(x, 3);
+        y = Math.Round(y, 3);
+        Console.WriteLine($"Точка пересечения двух прямых: х  ({x});у  ({y})");
+    }
+}
